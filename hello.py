48c76be0190f61d1d9807b030b8375d21d9983af
Kelvin Chuchu <mburukelvin17@gmail.com>
2024-03-21 04:29:59 +0300

    Added user view

--- hello.py
+++ hello.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
